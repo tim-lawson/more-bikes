af3def31e9c0dd751af1ba0310a4524ce1d9c1ae
Tim Lawson <qr23940@bristol.ac.uk>
2023-12-20 16:18:19 +0000

    t-test not wilcoxon

--- src/more_bikes/util/test.py
+++ src/more_bikes/util/test.py
@@ -1,27 +1,27 @@
 """Statistical tests."""
 
 from pandas import read_csv
-from scipy.stats import ranksums
+from scipy.stats import ttest_rel
 
 
 def test_task_1b(experiment1: str, experiment2: str):
-    """Wilcoxon rank-sum test."""
+    """Dependent t-test for paired samples."""
 
-    dataframe_a = read_csv(
+    dataframe1 = read_csv(
         f"more_bikes/experiments/task_1b/{experiment1}/{experiment1}_cv.csv"
     )
-    dataframe_b = read_csv(
+    dataframe2 = read_csv(
         f"more_bikes/experiments/task_1b/{experiment2}/{experiment2}_cv.csv"
     )
 
-    scores1 = dataframe_a["score"].values
-    scores2 = dataframe_b["score"].values
+    scores1 = dataframe1["score"].values
+    scores2 = dataframe2["score"].values
 
-    result = ranksums(scores1, scores2)
+    statistic, pvalue = ttest_rel(scores1, scores2)
 
-    print(f"{experiment1} vs {experiment2}")
-    print(f"statistic = {result.statistic:.3f}")
-    print(f"p-value = {result.pvalue:.3f}")
+    print(f"\n{experiment1} vs {experiment2}")
+    print(f"statistic = {statistic:.3f}")
+    print(f"p-value = {pvalue:.3f}")
 
 
 if __name__ == "__main__":
